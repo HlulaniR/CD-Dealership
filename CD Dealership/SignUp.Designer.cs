76b553c7b8186376cbdb89b203dca1bc60344650
HlulaniR <71436045+HlulaniR@users.noreply.github.com>
2021-09-16 14:29:37 +0200

    Add project files.

---
+++ CD Dealership/SignUp.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace CD_Dealership
+{
+	partial class Form2
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.nameTxt = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+			this.label1.Location = new System.Drawing.Point(206, 42);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(123, 29);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "New User";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.ForeColor = System.Drawing.Color.Red;
+			this.label2.Location = new System.Drawing.Point(145, 363);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(276, 17);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Please enter your details to register profile";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(81, 98);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(80, 17);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "First Name:";
+			// 
+			// nameTxt
+			// 
+			this.nameTxt.Location = new System.Drawing.Point(192, 95);
+			this.nameTxt.Name = "nameTxt";
+			this.nameTxt.Size = new System.Drawing.Size(210, 22);
+			this.nameTxt.TabIndex = 3;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(81, 142);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(80, 17);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "Last Name:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(192, 142);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(210, 22);
+			this.textBox1.TabIndex = 5;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 317);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(51, 27);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "Back";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form2
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(538, 405);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.nameTxt);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form2";
+			this.Text = "Sign Up";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox nameTxt;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+	}
+}
