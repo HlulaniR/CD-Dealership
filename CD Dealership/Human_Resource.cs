e93dc9f4596efdf8247d83a909767d176cbfbfb3
HlulaniR <71436045+HlulaniR@users.noreply.github.com>
2021-09-27 23:00:24 +0200

    Update

--- CD Dealership/Human_Resource.cs
+++ CD Dealership/Human_Resource.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Security.Permissions;
 
 namespace CD_Dealership
 {
@@ -15,6 +19,18 @@ namespace CD_Dealership
 		public Human_Resource()
 		{
 			InitializeComponent();
+		}
+		private SqlConnection con;
+		private SqlCommand comm;
+		private DataSet ds;
+		private SqlDataAdapter adp;
+		private string conStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Nhlul\Desktop\223 Project\CD Dealership\CD Dealership\CDManagementDB.mdf;Integrated Security = True"; //open our database
+		private const string readEmployees = "SELECt UserID, Email, Firstname, LastName, Gender, Phone_No, Position, Address  FROM Employees"; //read from our employee datatbase
+
+
+		private void Human_Resource_Load(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
---
+++ CD Dealership/dbo.Employees.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Employees] (
+    [UserID]           INT           NOT NULL,
+    [FirstName]    NVARCHAR (30) NULL,
+    [LastName]     NVARCHAR (30) NULL,
+    [Gender]       NCHAR (1)     NULL,
+    [Email]        NVARCHAR (30) NULL,
+    [Phone_No] NCHAR (10)    NULL,
+    [Address]      NVARCHAR (30) NULL,
+    [Position]     NVARCHAR (30) NULL,
+    PRIMARY KEY CLUSTERED ([UserID] ASC)
+);
+
